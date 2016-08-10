175e57860bde45b5e9ed38227e526320e096a555
Joanne Jungwon Jang <joanne.jang@stanford.edu>
2016-08-10 00:35:22 -0700

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -63,8 +63,42 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+		int size = list.size();
+		if (size <= 1) {
+			return list;
+		}
+		// make two lists with half the elements each.
+		List<T> first = mergeSort(new LinkedList<T>(list.subList(0, size/2)), comparator);
+		List<T> second = mergeSort(new LinkedList<T>(list.subList(size/2, size)), comparator);
+
+		return merge(first, second, comparator);
+	}
+
+	private List<T> merge(List<T> first, List<T> second, Comparator<T> comparator) {
+		List<T> result = new LinkedList<T>();
+		int total = first.size() + second.size();
+		for (int i = 0; i < total; i++) {
+			List<T> winner = compareTwo(first, second, comparator);
+			result.add(winner.remove(0));
+		}
+		return result;
+	}
+
+	private List<T> compareTwo(List<T> first, List<T> second, Comparator<T> comparator) {
+		if (first.size() == 0) {
+			return second;
+		}
+		if (second.size() == 0) {
+			return first;
+		}
+		int res = comparator.compare(first.get(0), second.get(0));
+		if (res < 0) {
+			return first;
+		}
+		if (res > 0) {
+			return second;
+		}
+		return first;
 	}
 
 	/**
@@ -75,7 +109,12 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+		PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+		heap.addAll(list);
+		list.clear();
+		while (heap.size() > 0) {
+			list.add(heap.poll());
+		}
 	}
 
 	
@@ -89,8 +128,23 @@ public class ListSorter<T> {
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+		PriorityQueue<T> heap = new PriorityQueue<T>(list.size(), comparator);
+		for (T element: list) {
+			if (heap.size() < k) {
+				heap.offer(element);
+				continue;
+			}
+			int compared = comparator.compare(element, heap.peek());
+			if (compared > 0) {
+				heap.poll();
+				heap.offer(element);
+			}
+		}
+		List<T> res = new ArrayList<T>();
+		while (heap.size() > 0) {
+			res.add(heap.poll());
+		}
+		return res;
 	}
 
 	
